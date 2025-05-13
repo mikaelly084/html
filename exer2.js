ef5130bb2abfbae0795ddf81080bf66cf0b5c90f
mikaelly084 <mikaellyalmeida084@gmail.com>
2025-05-13 11:35:08 -0300

    Add files via upload

---
+++ exer2.js
@@ -0,0 +1,15 @@
+function cliqueMouse() {
+    console.log("Clicou no mouse!!");
+    document.querySelector('#texto').innerText = "Clicou aqui!";
+}
+ 
+function tirouMouse() {
+    console.log("Tirou o Mouse!!");
+    document.querySelector('#texto').innerText = "Tirou o Mouse daqui";
+}
+ 
+function passouMouse() {
+    console.log("Passou o Mouse!!");
+ 
+    document.querySelector('#texto').innerText = "Passou o Mouse aqui";
+}
